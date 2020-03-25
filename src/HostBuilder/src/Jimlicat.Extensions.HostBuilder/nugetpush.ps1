9c34a3ce449388b02100aedcd9c1cd299ba26db8
jm6041 <jmmail@foxmail.com>
2020-03-25 16:16:23 +0800

    add ver

--- src/HostBuilder/src/Jimlicat.Extensions.HostBuilder/nugetpush.ps1
+++ src/HostBuilder/src/Jimlicat.Extensions.HostBuilder/nugetpush.ps1
@@ -3,7 +3,7 @@ $pkgDir = "publish\nupkgs"
 $pkgItem = Get-ChildItem -Path $pkgDir -Filter *.nupkg | Sort-Object -Property Name | Select-Object -Last 1
 if($pkgItem){
     Write-Output "pkg file:$($pkgItem.FullName)"
-    dotnet.exe nuget push -k yx1234 -s http://tfs:8088/nuget $pkgItem.FullName
+    dotnet.exe nuget push $pkgItem.FullName -k oy2cv77aceiah6acry6aqblnrl2tq5qudjohelycsym534 -s https://api.nuget.org/v3/index.json
 }else {
     Write-Output "pkg file is empty!"
 }
