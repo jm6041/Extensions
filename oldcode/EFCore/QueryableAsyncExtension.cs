710b2224c217012b7d777cab86292a93343c8d85
jm6041 <jmmail@foxmail.com>
2021-11-27 00:19:46 +0800

    add old code.

---
+++ oldcode/EFCore/QueryableAsyncExtension.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Jimlicat.Utils;
+
+namespace Microsoft.EntityFrameworkCore
+{
+    /// <summary>
+    /// 分页异步扩展
+    /// </summary>
+    public static class QueryableAsyncExtension
+    {
+        /// <summary>
+        /// 从全量数据生成分页数据
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="page">分页数据</param>
+        /// <param name="cancellationToken"><see cref="CancellationToken"/></param>
+        /// <returns></returns>
+        public static async Task<DataResult<T>> ToPagedResultAsync<T>(this IQueryable<T> source, PageParameter page, CancellationToken cancellationToken = default)
+        {
+            Checker.NotNull(source, nameof(source));
+            Checker.CheckPageParameter(page, nameof(page));
+            int count = await source.CountAsync(cancellationToken);
+            if (page.PageSize <= 0)
+            {
+                return new DataResult<T>() { Count = count, Result = Enumerable.Empty<T>().ToList(), };
+            }
+            IList<T> result = await source.Page(page).ToListAsync(cancellationToken);
+            return new DataResult<T>() { Count = count, Result = result, };
+        }
+
+        /// <summary>
+        /// 从全量数据生成OData数据
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="para">分页数据</param>
+        /// <param name="cancellationToken"><see cref="CancellationToken"/></param>
+        /// <returns></returns>
+        public static async Task<DataResult<T>> ToODataResultAsync<T>(this IQueryable<T> source, IODataParameter para, CancellationToken cancellationToken = default)
+        {
+            Checker.NotNull(source, nameof(source));
+            Checker.CheckODataParameter(para, nameof(para));
+            int count = 0;
+            if (para.Count)
+            {
+                count = await source.CountAsync(cancellationToken);
+            }
+            if (para.Top <= 0)
+            {
+                return new DataResult<T>() { Count = count, Result = Enumerable.Empty<T>().ToList(), };
+            }
+            IList<T> result = await source.ODataQuery(para).ToListAsync(cancellationToken);
+            return new DataResult<T>() { Count = count, Result = result, };
+        }
+    }
+}
