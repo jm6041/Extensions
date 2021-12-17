d861cc6fb13a8dadf209a295bc69031a48d99f72
jm6041 <jmmail@foxmail.com>
2021-12-17 11:04:25 +0800

    add un exception handler.

--- tools/FileTools/src/FileTools/App.xaml.cs
+++ tools/FileTools/src/FileTools/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +13,15 @@ namespace FileTools
     /// </summary>
     public partial class App : Application
     {
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            AppDomain.CurrentDomain.UnhandledException += (sender, error) =>
+            {
+                MessageBox.Show(error.ExceptionObject.ToString(), "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+            };
+
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
     }
 }
